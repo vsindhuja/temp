6bb6989244e5cc442c95805eda67d9d35d9dcc32
akshayvish <akshay.vish@gmail.com>
2012-11-21 23:41:54 -0500

    Testing Commit

---
+++ Testing/TCPHandshake.java
@@ -0,0 +1,421 @@
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
+
+class Server extends Thread{
+
+	public static ServerSocket tcpservsock;
+	public Socket socket;
+
+	public Server (Socket socket)
+	{
+		this.socket = socket;
+	}
+
+	public Server(int tcpport) throws IOException
+	{
+		try{
+			tcpservsock = new ServerSocket (tcpport);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Exception on new Server Socket"+e);
+		}
+	}
+
+	public void run(){
+		Socket socket;
+		try{
+			while(true){
+				socket = tcpservsock.accept();  // accept connection
+
+				PrintStream output = new PrintStream(socket.getOutputStream());
+				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String incomingMess = "";
+				while((incomingMess = input.readLine())!=null){
+					if(incomingMess.startsWith("SIMPELLA CONNECT/0.6")){
+						//Check for existing connection count to be introduced.
+						Boolean connAcceptStatus = true;
+						if(connAcceptStatus){
+							System.out.println("Received " + incomingMess + " from client, sending response." + Util.CONNECTION_ACCEPTED);
+							output.println(Util.CONNECTION_ACCEPTED);
+							System.out.println("Echoer>>");
+						}else{ 
+							System.out.println("Received " + incomingMess + " from client, sending response." + Util.CONNECTION_REFUSED);
+							output.println(Util.CONNECTION_REFUSED);
+						}
+					}
+					else if(incomingMess.equalsIgnoreCase("done")){
+						System.out.println("Handshake SUCCESSFUL!!!");
+						System.out.print("Echoer>>");
+						output.println("Successful");
+					}
+				}
+				new ClientHandler(socket).start();
+			}
+		}catch (IOException ioe){
+			//Do nothing, since this exception is coming from the BufferedReader's reset method call.
+			System.out.print("");
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+class ClientHandler extends Thread{
+	private Socket sock;
+	PrintStream output ;
+	BufferedReader input ;
+
+	ClientHandler(Socket sock){
+		this.sock = sock;
+	}
+
+	public void run(){
+		String line;
+		try{
+			output = new PrintStream(sock.getOutputStream());
+			input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			while(true){
+				while((line=input.readLine())!=null){
+					System.out.println(line);
+					output.println("Welcome to server");
+					output.println("Received : " + line);
+				}
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+class Client extends Thread
+{
+	Socket sock;
+	String ipAddress;
+	String hostName;
+	int localPort;
+	int remotePort;
+	String message;
+	Integer conID;
+	DataInputStream dis;
+	PrintStream os;
+	boolean send = false; 
+
+	public int getLocalPort() {
+		return localPort;
+	}
+
+	public void setLocalPort(int localPort) {
+		this.localPort = localPort;
+	}
+
+	public int getRemotePort() {
+		return remotePort;
+	}
+
+	public void setRemotePort(int remotePort) {
+		this.remotePort = remotePort;
+	}
+
+	public Integer getConID() {
+		return conID;
+	}
+
+	public void setConID(Integer conID) {
+		this.conID = conID;
+	}
+
+	public DataInputStream getDis() {
+		return dis;
+	}
+
+	public void setDis(DataInputStream dis) {
+		this.dis = dis;
+	}
+
+	public PrintStream getOs() {
+		return os;
+	}
+
+	public void setOs(PrintStream os) {
+		this.os = os;
+	}
+
+	public boolean isSend() {
+		return send;
+	}
+
+	public void setSend(boolean send) {
+		this.send = send;
+	}
+
+	public void setSock(Socket sock) {
+		this.sock = sock;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public Socket getSock() {
+		return sock;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public Client(){
+		//Default constructor.
+	}
+
+	public void send(String message){
+
+		Socket tempClientSock = sock; 
+		try {
+			PrintStream clientOutput = new PrintStream(tempClientSock.getOutputStream());
+			BufferedReader clientInputLine = new BufferedReader(new InputStreamReader(tempClientSock.getInputStream()));
+			clientOutput.println(message);
+			System.out.println("Response from server $$$$$ " + clientInputLine.readLine());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void handShake(){
+		if(TCPHandshake.handshake){
+			Socket tempClientSock = sock; 
+			String inputline = "";
+			try {
+				PrintStream clientOutput = new PrintStream(tempClientSock.getOutputStream());
+				BufferedReader clientInputLine = null;
+				clientInputLine = new BufferedReader(new InputStreamReader(tempClientSock.getInputStream()));
+				clientInputLine.mark(0);
+				System.out.println("Initiating handshake by sending connection request to client");
+				clientOutput.println("SIMPELLA CONNECT/0.6 \r \n");
+					while((inputline=clientInputLine.readLine())!=null){
+						if(inputline.startsWith("SIMPELLA/0.6")){
+							if(inputline.substring(13,16).equals("200")){
+								//System.out.println("Receieved "+ inputline +" from server, Connection ACCEPTED.");
+								System.out.println(inputline);
+							}else if(inputline.substring(13,16).equals("503")){
+								//System.out.println("Receieved "+ inputline +" from server, Connection REFUSED.");
+								System.out.println(inputline);
+							}
+							//clientOutput.println("Done");
+							TCPHandshake.handshake=false;
+							System.out.println("Echoer>>");
+							//clientInputLine.reset();
+						}
+						if(inputline.equalsIgnoreCase("Successful")){
+							TCPHandshake.handshake=false;
+							clientInputLine.reset();
+						}
+					}
+			} catch (IOException e) {
+				//Do nothing, since this exception is coming from the BufferedReader's reset method call.
+				System.out.print("");
+			}
+		}
+	}
+}
+
+public class TCPHandshake
+{
+	static int connCount = 0;
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static HashMap<Integer, Thread> threadMap = new HashMap<Integer, Thread>();
+
+	static HashMap<Integer,Client> hmClients = new HashMap<Integer, Client>();
+
+	public static Boolean handshake = false;
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args)
+	{
+
+		int tcpport = Integer.parseInt(args[0]);
+
+		try{
+
+			Thread t = new Server(tcpport);
+			t.start();
+
+		}
+		catch (IOException e)
+		{
+			System.out.println("Exception on new Server Socket"+e);
+		}
+
+		String ipAddr;
+		int tcpPort = 0000;
+		String input;
+		//Create sockets
+
+		while(true){
+			System.out.print("Echoer>>");
+			if(scan.hasNext()){
+				input = scan.nextLine();
+				if(input.substring(0,4).equalsIgnoreCase("info")){
+					try
+					{ 
+						Socket sock = new Socket("8.8.8.8", 53);
+						System.out.println (String.format("%10s%10s%10s%10s","IP","Hostname","TCP port"));
+						System.out.println ("-------------------------------------------------------------------------------------------------");
+						InetAddress ipaddr = sock.getLocalAddress();
+						System.out.print((String.format("%10s",ipaddr)));
+						System.out.print((String.format("%10s",ipaddr.getLocalHost().getHostName())));
+						System.out.println((String.format("%10d",tcpport)));
+					}
+					catch (IOException e) {
+						System.out.println(e.getMessage());
+					} 
+				}
+
+				else if(input.substring(0,4).equalsIgnoreCase("show")){
+					System.out.println (String.format("%10s%10s%20s%20s%20s","conn. ID","IP","Hostname","local port","remote port"));
+					System.out.println ("-------------------------------------------------------------------------------------------------");
+					for(int i=0;i<=hmClients.size();i++){
+						Client tempInfo = hmClients.get(i);
+						if(tempInfo!=null){
+							System.out.print(String.format("%-10d",i));
+							System.out.print((String.format("%10s",tempInfo.getIpAddress())));
+							System.out.print((String.format("%20s",tempInfo.getHostName())));
+							System.out.print((String.format("%20d",tempInfo.getLocalPort())));
+							System.out.println((String.format("%20d",tempInfo.getRemotePort())));
+
+						}
+					}
+				}else if(input.substring(0,6).equalsIgnoreCase("sendto")){
+					String[] splitArr ;
+					try
+					{
+						if(input.substring(7)!=null){
+							splitArr = input.substring(7).split(" ");
+							int port = Integer.parseInt(splitArr[1]);
+							InetAddress IPAddress = InetAddress.getByName(splitArr[0]);
+							String message = "";
+							for(int i=2;i<splitArr.length;i++){
+								message = splitArr[2] + " ";
+							}
+							DatagramSocket clientSocket = new DatagramSocket();
+							byte[] sendData = new byte[1024];
+							sendData = message.getBytes();
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+							clientSocket.send(sendPacket);
+							clientSocket.close();
+
+						}
+					}	catch (UnknownHostException e) {
+						System.out.println(e.getMessage());
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					} 
+
+				}else if(input.substring(0,4).equalsIgnoreCase("send")){
+					String[] splitArr ;
+					if(input.substring(5)!=null){
+						splitArr = input.substring(5).split(" ");
+						int conID = Integer.parseInt(splitArr[0]);
+						String message = "";
+						for(int i=1;i<splitArr.length;i++){
+							message = splitArr[1] + " ";
+						}
+						System.out.println("Message : " + message);
+						//Call the object of Info corresponding to the conID and set the message variable value.
+						(hmClients.get(conID)).setMessage(message);
+						System.out.println("Thread state ::: "+ (threadMap.get(conID)).getState());
+						Socket tempClientSock = (hmClients.get(conID)).getSock(); 
+						try {
+							PrintStream clientOutput = new PrintStream(tempClientSock.getOutputStream());
+							BufferedReader clientInputLine = new BufferedReader(new InputStreamReader(tempClientSock.getInputStream()));
+							clientOutput.println(message);
+							System.out.println("Response from server $$$$$ " + clientInputLine.readLine());
+							//clientOutput.print(new Client());
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}else{
+						System.out.println("More parameters required to run the command.");
+					}
+				}else if(input.substring(0,7).equalsIgnoreCase("Connect")){
+					try {
+						String[] splitArr = input.substring(8).split(" ");
+
+						ipAddr = splitArr[0];
+						//Trim the last PORT number from the input string.
+						tcpPort = Integer.parseInt(splitArr[1].trim());
+						Socket sock = new Socket(ipAddr, tcpPort);
+						//For each socket we create and start off a new thread. Take-id and go! #russelpeters
+						connCount = connCount + 1;
+						Client newCli = new Client();
+						newCli.setSock(sock);
+						newCli.setIpAddress(ipAddr);
+						newCli.setConID(connCount);
+						newCli.setLocalPort(tcpPort);
+						handshake = true;
+						newCli.handShake();
+						hmClients.put(connCount, newCli);
+						Thread t = new Thread(newCli);
+
+						threadMap.put(connCount, t);
+						t.start();
+
+					} catch (IndexOutOfBoundsException ie){
+						System.out.println(" Input needs more parameters. ");
+					} catch (BindException be){
+						System.out.println("Address already in use");
+					} catch (ConnectException ce){
+						System.out.println(" No listner for the port "+ tcpPort + " found. Please activate the listener.");
+					} catch (UnknownHostException e) {
+						System.out.println(e.getMessage());
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					} 
+				}else
+					if(input.substring(0, 10).equalsIgnoreCase("disconnect")){
+						if(!(input.substring(10)).isEmpty()){
+							int conID = Integer.parseInt(input.substring(10).trim());
+							try {
+								//Close the socket opened against the connection ID and remove it from the HashMap.
+								Socket tempClientSock = (hmClients.get(conID)).getSock();
+								PrintStream clientOutput = new PrintStream(tempClientSock.getOutputStream());
+								clientOutput.println("Disconnected from "+tempClientSock.getLocalSocketAddress());
+								hmClients.get(conID).getSock().close();
+								hmClients.remove(conID);
+								TCPHandshake.connCount = TCPHandshake.connCount - 1;
+								//Also stop the thread that was associated with it and delete it from the HashMan.
+								TCPHandshake.threadMap.remove(conID);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					else{
+						//I wanted to write "Go do gangnam style" but guess i'm going to have to be happy with just this.
+						System.out.println("Incorrect Input");
+					}
+			}
+		}
+	}	
+}
---
+++ Testing/Util.java
@@ -0,0 +1,6 @@
+public class Util {
+
+	public static String CONNECT = "SIMPELLA CONNECT/0.6 \r \n";
+	public static String CONNECTION_ACCEPTED = "SIMPELLA/0.6 200 OK \r \n";
+	public static String CONNECTION_REFUSED = "SIMPELLA/0.6 503 Exceeded Connection Capacity \r \n";
+}
