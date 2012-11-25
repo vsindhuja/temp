6bc787b5fb8dfba85919c621171306c165facae8
Sindhuja Venkatesh <v_sindhuja88@yahoo.co.in>
2012-11-25 16:41:53 -0500

    adding msg files

---
+++ Simpella-Akki/SimpellaTest/src/simpella/QueryMessage.java
@@ -0,0 +1,20 @@
+package simpella;
+
+public class QueryMessage {
+
+	private byte minSpeed;
+	private String searchString = "";
+	
+	public byte getMinSpeed() {
+		return minSpeed;
+	}
+	public void setMinSpeed(byte minSpeed) {
+		this.minSpeed = minSpeed;
+	}
+	public String getSearchString() {
+		return searchString;
+	}
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+}
